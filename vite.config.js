d013f7eca7d35b712b86d918b0fadc0059a17ff9
Sam Lee <cable8mm@gmail.com>
2024-04-10 00:41:49 +1200

    Fit to build as vite style

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     root: 'src',
+    publicDir: '../public',
     build: {
-        outDir: '../public',
-        emptyOutDir: true
+        outDir: '../dist'
     },
     test: {
         root: 'tests'
